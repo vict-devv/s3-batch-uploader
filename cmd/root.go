fc297f962e5aaf2188c232a552232b0991801bc9
Victor <victor.devv@gmail.com>
2023-05-21 15:06:48 -0300

    fix: lint fixes

--- cmd/root.go
+++ cmd/root.go
@@ -24,7 +24,7 @@ var (
 var rootCmd = &cobra.Command{
 	Use:   "s3-batch-uploader",
 	Short: "S3 Batch Uploader is a simple tool that helps you perform upload of folders to a AWS S3 bucket",
-	Long: `A Go commmand line tool that performs upload of folders to AWS S3 buckets, built with Go Cobra library
+	Long: `A Go command line tool that performs upload of folders to AWS S3 buckets, built with Go Cobra library
 			to make it powerful and easy to extend. For more information access the README
 			at my GitHub: https://github.com/vict-devv/s3-batch-uploader`,
 	Version: Version,
@@ -53,21 +53,21 @@ var rootCmd = &cobra.Command{
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&AwsProfile, "profile", "p", constants.AwsDefaultProfile, "AWS credential profile to be used (required)")
-	rootCmd.MarkPersistentFlagRequired("profile")
+	_ = rootCmd.MarkPersistentFlagRequired("profile")
 
 	rootCmd.PersistentFlags().StringVarP(&AwsRegion, "region", "r", constants.AwsDefaultRegion, "The bucket region (required)")
-	rootCmd.MarkPersistentFlagRequired("region")
+	_ = rootCmd.MarkPersistentFlagRequired("region")
 
 	rootCmd.PersistentFlags().StringVarP(&AwsBucket, "bucket", "b", "", "The AWS S3 destination bucket (required)")
-	rootCmd.MarkPersistentFlagRequired("folder")
+	_ = rootCmd.MarkPersistentFlagRequired("folder")
 
 	rootCmd.PersistentFlags().StringVarP(&Folder, "folder", "f", constants.DefaultFolder, "The path of the folder to be upload (required)")
-	rootCmd.MarkPersistentFlagRequired("folder")
+	_ = rootCmd.MarkPersistentFlagRequired("folder")
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprint(os.Stderr, err)
+		_, _ = fmt.Fprint(os.Stderr, err)
 		os.Exit(1)
 	}
 }
